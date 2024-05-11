eb57508cc1cdee37fb3d899df1d0dff56b4c66fe
Yunus Emre Topal <emretopal20@gmail.com>
2024-05-11 22:41:10 +0200

    player animations and attack checks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    [Header("Attack Directions")]
+    [SerializeField] private GameObject attackBack;
+    [SerializeField] private GameObject attackFront;
+    [SerializeField] private GameObject attackLeft;
+    [SerializeField] private GameObject attackRight;
+    
+    
+    [Header("Stats")]
     [SerializeField] private float speed;
+    [SerializeField] private float attackCooldown = 0.5f;
+    
+    private bool canAttack = true;
     
     
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
+    private Animator _animator;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,10 +35,61 @@ public class PlayerMovement : MonoBehaviour {
 
         Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * speed;
 
-        rb.velocity = movement;
+        _rb.velocity = movement;
+        
+        _animator.SetFloat("Speed", movement.magnitude);
+
+        if(movement != Vector2.zero){
+            _animator.SetFloat("Horizontal", movement.x);
+            _animator.SetFloat("Vertical", movement.y);
+        }
+        
+        if(Input.GetKey(KeyCode.Mouse0) && canAttack) {
+            _animator.SetTrigger("attack_trigger");
+            canAttack = false;
+        }
+        
+        if(!canAttack){
+            attackCooldown -= Time.deltaTime;
+            if(attackCooldown <= 0){
+                canAttack = true;
+                attackCooldown = 0.5f;
+            }
+        }
+        
     }
     
     void OnCollisionEnter2D(Collision2D collision){
         Debug.Log("Collision with " + collision.gameObject.name);
+    }
+
+    public void Attack(int attackDirection) {
+        Collider2D[] hitColliders;
+        switch (attackDirection) {
+            case 0:
+                // take 0.6 radius around the back 
+                hitColliders = Physics2D.OverlapCircleAll(attackBack.transform.position, 0.6f);
+                break;
+            case 1:
+                // take 0.6 radius around the right
+                hitColliders = Physics2D.OverlapCircleAll(attackRight.transform.position, 0.6f);
+                break;
+            case 2:
+                // take 0.6 radius around the front
+                hitColliders = Physics2D.OverlapCircleAll(attackFront.transform.position, 0.6f);
+                break;
+            case 3:
+                // take 0.6 radius around the left
+                hitColliders = Physics2D.OverlapCircleAll(attackLeft.transform.position, 0.6f);
+                break;
+            default:
+                hitColliders = Array.Empty<Collider2D>();
+                break;
+        }
+
+        foreach (var collider in hitColliders) {
+            // TODO: Add damage to the enemy
+        }
+        
     }
 }
